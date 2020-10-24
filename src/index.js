3c7d2557a762219cab48145b2cada67b70355e9a
Farigus <52212018+Farigus@users.noreply.github.com>
2020-10-24 11:54:43 +0300

    feat: add my code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+
+    let num = n.toString().split("");
+
+    if (num.indexOf('-') !== -1) {
+
+        num.shift();
+
+    }
+
+    num = num.reverse();
+
+    num = num.join('');
+
+    return num;
+
 }
